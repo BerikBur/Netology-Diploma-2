ced8f26a3ba254c2b147a3ae945e5fd9f00e4b3a
Bair <ber89ber89@gmail.com>
2024-05-23 20:16:28 +0300

    Formal changes. Burger-menu added (JS-code will be modified)

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,12 @@
 	class Menu {
 		constructor(domNode) {
 			this.menu = domNode
-			this.menuButton = this.menu.querySelector('.js-menu__button')
+			this.menuButton = this.menu.querySelector('.js-menu_button')
 			this.menuButton.addEventListener('click', this.toggle.bind(this))
 		}
 
 		// Toggle the menu visibility
 		toggle() {
-			//console.log(this);
 			return this.menu.classList.toggle('js-menu_active')
 		}
 	}
@@ -21,3 +20,48 @@
 		new Menu(item)
 	})
 })()
+
+// Select the .header_top element
+const headerTop = document.querySelector('.header_top')
+// Select the .menu element
+const menu = document.querySelector('.menu')
+// Select the .menu element
+const menuList = document.querySelector('.menu_list')
+// Select the button element to toggle alignment
+const alignButton = document.querySelector('#alignButton')
+let isAligned = true // Variable to track the current alignment state
+// Function to toggle the alignment of .header_top element
+function toggleAlignment() {
+	if (isAligned) {
+		headerTop.style.alignItems = 'start' // Change the alignment to start
+		menu.style.display = 'flex' // Make sure the element is set to display: flex
+		menu.style.flexDirection = 'row-reverse' // Set flex-direction to row-reverse
+		menu.style.alignItems = 'start'
+
+		menuList.style.display = 'flex' // Make sure the element is set to display: flex
+		menuList.style.alignItems = 'start' // Set flex-direction to row-reverse
+		isAligned = false // Update the alignment state
+	} else {
+		headerTop.style.alignItems = 'center' // Restore the initial alignment to center
+
+		menuList.style.display = 'flex' // Make sure the element is set to display: flex
+		menuList.style.flexDirection = 'column' // Set flex-direction to row-reverse
+		menuList.style.marginLeft = 0
+
+		menu.style.alignItems = 'center'
+
+		isAligned = true // Update the alignment state
+	}
+}
+// Event listener for the button click to toggle alignment
+alignButton.addEventListener('click', toggleAlignment)
+
+const menuItems = document.querySelectorAll('.menu_list_pad')
+
+// Function to toggle the 'open' class on menu items for smooth opening
+function toggleMenuItems() {
+	menuItems.forEach(item => item.classList.toggle('open'))
+}
+
+// Event listener for clicking on the menu button to toggle menu items
+alignButton.addEventListener('click', toggleMenuItems)
